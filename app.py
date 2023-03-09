7f82ccdc57c7cd2d56a2f0b3273401cb6f8ca79e
Seongbum Seo <seo@seongbum.com>
2023-03-09 14:55:10 +0900

    Add template

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():  # put application's code here
     print("python flask server run")
     return render_template('trajectory.html')
 
+@app.route('/main')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
