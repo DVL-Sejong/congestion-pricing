427563c7a0652a45a06d7b3b3f74003e22f9ce14
Seongbum Seo <seo@seongbum.com>
2023-03-14 21:40:47 +0900

    Add parallel coordinates

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-import numpy as np
 import pandas as pd
-import sys
+import csv
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    print("python flask server run")
-    return render_template('trajectory.html')
-
-@app.route('/main')
 def index():
     return render_template('index.html')
+
+@app.route('/data/congestion/roads')
+def data_congestion_roads():
+    data = csv_to_list("static/data/bAllSpeedData_Transposed.csv")
+    return data
+
+@app.route('/data/congestion/roads/<int:day>')
+def data_congestion_roads_day(day: int):
+    data = csv_to_list("static/data/bAllSpeedData_Transposed.csv")
+    df = pd.DataFrame(data).set_index("road_id")
+    df_day = df[df.columns[day*24:(day+1)*24]]
+    df_day.columns = range(0, 24)
+    #return df_day.to_json() # {"시간(h)": {"도로번호": "속도(km/h)", ...}}
+    return df_day.to_csv()
+
+def csv_to_list(filename: str) -> list[dict]:
+    with open(filename, "r") as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+        return data
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/layouts/zones.js
+++ static/js/layouts/zones.js
@@ -1,10 +1,10 @@
-d3.csv("/static/data/bAllSpeedData.csv", renderParallelCoordinates);
+d3.csv("/data/congestion/roads/0", renderParallelCoordinates);
 
 function renderParallelCoordinates(data) {
     // set the dimensions and margins of the graph
-    var margin = { top: 30, right: 10, bottom: 10, left: 0 },
-        width = 280 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    var margin = { top: 5, right: 0, bottom: 10, left: 0 },
+        width = 290 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     var svg = d3.select("#average-tci")
@@ -16,15 +16,18 @@ function renderParallelCoordinates(data) {
             "translate(" + margin.left + "," + margin.top + ")");
 
     // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
-    dimensions = d3.keys(data[0]).filter(function (d) { return d != "Species" })
+    dimensions = d3.keys(data[0]).filter(function (d) { return d != "road_id" })
 
     // For each dimension, I build a linear scale. I store all in a y object
+    const min = d3.min(dimensions, i => d3.min(data, d => +d[i]));
+    const max = d3.max(dimensions, i => d3.max(data, d => +d[i]));
     var y = {}
     for (i in dimensions) {
-        name = dimensions[i]
-        y[name] = d3.scaleLinear()
-            .domain(d3.extent(data, function (d) { return +d[name]; }))
-            .range([height, 0])
+        road_id = dimensions[i]
+        y[road_id] = d3.scaleLinear()
+            //.domain(d3.extent(data, function (d) { return +d[road_id]; }))
+            .domain([min, max])
+            .range([height, 0]);
     }
 
     // Build the X scale -> it find the best position for each Y axis
@@ -45,7 +48,7 @@ function renderParallelCoordinates(data) {
         .enter().append("path")
         .attr("d", path)
         .style("fill", "none")
-        .style("stroke", "#69b3a2")
+        .style("stroke", "steelblue")
         .style("opacity", 0.5)
 
     // Draw the axis:
@@ -56,11 +59,11 @@ function renderParallelCoordinates(data) {
         // I translate this element to its right position on the x axis
         .attr("transform", function (d) { return "translate(" + x(d) + ")"; })
         // And I build the axis with the call function
-        .each(function (d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+        //.each(function (d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
         // Add axis title
         .append("text")
         .style("text-anchor", "middle")
-        .attr("y", -9)
+        .attr("y", -5)
         .text(function (d) { return d; })
         .style("fill", "black")
 }
